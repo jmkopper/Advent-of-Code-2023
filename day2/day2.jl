8ea4ed5c112774113938502f8a974ba1fe6cd225
jmkopper <jay.kopper@gmail.com>
2023-12-05 15:40:00 -0500

    Create day2.jl

---
+++ day2/day2.jl
@@ -0,0 +1,21 @@
+function parseRow(row)
+    vals = split(split(row, ":")[2], [',', ';']) .|> strip
+    rgb = Dict("red" => 0, "blue" => 0, "green" => 0)
+    for v in vals
+        s = split(v)
+        color, count = s[2], parse(Int, s[1])
+        rgb[color] = max(count, rgb[color])
+    end
+    return rgb
+end
+
+validRow(rgb) = rgb["red"] <= 12 && rgb["green"] <= 13 && rgb["blue"] <= 14
+
+function main()
+    input = readlines("day2/input.txt")
+    m = [parseRow(row) for row in input]
+    println("Part 1: ", (1:length(input))[validRow.(m)] |> sum)
+    println("Part 2: ", sum(x["red"] * x["green"] * x["blue"] for x in m))
+end
+
+main()
