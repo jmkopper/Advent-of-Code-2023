ae20760dfe97f478fca99ea7820cdc116eb1ef79
jmkopper <jay.kopper@gmail.com>
2023-12-07 12:44:51 -0500

    Update day7.jl

--- day7/day7.jl
+++ day7/day7.jl
@@ -9,12 +9,8 @@ struct OptimizedHand
     new::Hand
 end
 
-const VALS = Dict(string(i) => i for i in 2:9)
-VALS["T"] = 10
-VALS["J"] = 1
-VALS["Q"] = 12
-VALS["K"] = 13
-VALS["A"] = 14
+const VALS1 = Dict(zip([[string(i) for i in 2:9]; ["T", "J", "Q", "K", "A"]], 2:14))
+const VALS2 = Dict(zip([[string(i) for i in 2:9]; ["T", "J", "Q", "K", "A"]], [2:9; [10, 1, 12, 13, 14]]))
 
 function handType(h::Hand)::Int
     counts = Dict(i => length(h.cards[h.cards .== i]) for i in h.cards)
@@ -25,18 +21,18 @@ function handType(h::Hand)::Int
     return 0
 end
 
-function cmpCards(a::Vector{String}, b::Vector{String})::Bool
-    x, y = [VALS[c] for c in a], [VALS[c] for c in b]
+function cmpCards(a::Vector{String}, b::Vector{String}, vals)::Bool
+    x, y = [vals[c] for c in a], [vals[c] for c in b]
     m = x .!= y
     return any(m) ? x[m][1] < y[m][1] : false
 end
 
 function Base.isless(a::Hand, b::Hand)::Bool
-    return handType(a) == handType(b) ? cmpCards(a.cards, b.cards) : handType(a) < handType(b)
+    return handType(a) == handType(b) ? cmpCards(a.cards, b.cards, VALS1) : handType(a) < handType(b)
 end
 
 function Base.isless(a::OptimizedHand, b::OptimizedHand)::Bool
-    return handType(a.new) == handType(b.new) ? cmpCards(a.old.cards, b.old.cards) : handType(a.new) < handType(b.new)
+    return handType(a.new) == handType(b.new) ? cmpCards(a.old.cards, b.old.cards, VALS2) : handType(a.new) < handType(b.new)
 end
 
 function optimizeHand(h::Hand)::OptimizedHand
@@ -48,7 +44,7 @@ function optimizeHand(h::Hand)::OptimizedHand
     top = collect(keys(counts))[argmax(collect(values(counts)))]
     new = Hand(copy(h.cards))
     new.cards[new.cards .== "J"] .= top
-    
+
     return OptimizedHand(h, new)
 end
 
@@ -57,8 +53,9 @@ function main()
     hands = [Hand(split(line, " ")[1] |> x -> split(x, "")) for line in lines]
     bids = [split(line, " ")[2] for line in lines] .|>  x-> parse(Int, x)
     besthands = [optimizeHand(h) for h in hands]
-    order = sortperm(besthands)
-    p2 = sum(x * y for (x, y) in enumerate(bids[order]))
+    p1 = sum(x * y for (x, y) in enumerate(bids[sortperm(hands)]))
+    println("Part 1: ", p1)
+    p2 = sum(x * y for (x, y) in enumerate(bids[sortperm(besthands)]))
     println("Part 2: ", p2)
 end
 
