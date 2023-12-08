a1a8e918c290b22356ea23633bd61e103ce2f942
jmkopper <jay.kopper@gmail.com>
2023-12-08 11:23:44 -0500

    Create day8.jl

---
+++ day8/day8.jl
@@ -0,0 +1,51 @@
+function makeNodes(input)
+    nodes = Dict(line[1:3] => (line[8:10], line[13:15]) for line in input)
+    return nodes
+end
+
+function part1(nodes, start, stop, dirs)
+    steps, loops = 0, 0
+    m = length(dirs)
+    current = start
+    while current != stop
+        if steps == m
+            steps, loops = 0, loops + 1
+        end
+        i = steps + 1
+        current = dirs[i] == "L" ? nodes[current][1] : nodes[current][2]
+        steps += 1
+    end
+    return steps + loops * m
+end
+
+function part2(nodes, anodes, dirs)
+    m, d = length(dirs), []
+    for a in anodes
+        current = a
+        loops, steps = 0, 0
+        while current[3] != 'Z'
+            if steps == m
+                steps, loops = 0, loops + 1
+            end
+            i = steps + 1
+            current = dirs[i] == "L" ? nodes[current][1] : nodes[current][2]
+            steps += 1
+        end
+        push!(d, loops + 1)
+    end
+    return lcm(d...) * m
+end
+
+function main()
+    input = readlines("day8/input.txt")
+    dirs = split(input[1], "")
+    nodes = makeNodes(input[3:end])
+
+    println("Part 1: ", part1(nodes, "AAA", "ZZZ", dirs))
+
+    anodes = filter(n -> n[3] == 'A', keys(nodes)) |> collect
+    x = part2(nodes, anodes, dirs)
+    println("Part 2: ", x)
+end
+
+main()
